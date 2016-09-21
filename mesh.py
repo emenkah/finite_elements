6c526ba0ca7eba5f421a00001e23c2926712d0b2
James Vance <jnvance@up.edu.ph>
2016-09-21 15:23:17 +0200

    Initial work on mesh.py

--- mesh.py
+++ mesh.py
@@ -4,6 +4,35 @@ def read_msh(filename):
 
     """ read mesh code """
 
+    x = np.array([])
+    y = np.array([])
+
+    f = open(filename,'r')
+
+    for line in f:
+        if line[0] == '$':
+            print "this is useless"
+        else:
+            l = map(float,line.split())
+            # print(map(float,line.split())) # convert string to list of floats
+            if len(l) == 4:
+                x = np.append(x,l[1])
+                y = np.append(y,l[2])
+            if len(l) == 7:
+
+            if len(l) == 8:
+
+
+    print(x)
+    print(y)
+
+    nodes = None
+    b_nodes = None
+
+
+
+    f.close()
+
     r_id = 0
     for row in topo:
         ck =      (x[row[1]]-x[row[0]])*(y[row[2]]-y[row[0]])
